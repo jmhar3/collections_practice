8e9fe326301a7cd1535ef456af7925d45318be51
jmhar <jmhar@protonmail.com>
2021-09-01 16:42:23 +1000

    update

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc array
+    array.sort
+end
 
+def sort_array_desc array
+    array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count array
+    array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements array
+    array [1], array [2] = array [2], array [1] 
+    array
+end
+
+def swap_elements_from_to array, index, destination_index
+    array [index], array [destination_index] = array [destination_index], array [index] 
+    array
+end
+
+def reverse_array array
+    new_array = array.reverse
+end
+
+def kesha_maker array
+    array.each { |word| word[2] = "$" }
+end
+
+def find_a array
+    array.select { |item| item.start_with?("a") }
+end
+
+def sum_array array
+    array.inject{ |num1, num2| num1 + num2 }
+end
+
+# def add_s array
+#     array.collect { |word| array[1] == word ? word : word + "s" }
+# end
+
+def add_s array
+    array.each_with_index.collect { |word, i| i != 1 ? word + "s" : word }
+end
